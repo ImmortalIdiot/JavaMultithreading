1066d74e66b7f53335e0bf098df068a81b3c3962
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-18 23:14:28 +0300

    feat: create bounded buffer

---
+++ src/buffer/BoundedBuffer.java
@@ -0,0 +1,43 @@
+package buffer;
+
+class BoundedBuffer<T> {
+    private final T[] buffer;
+    private int count = 0;
+    private int in = 0;
+    private int out = 0;
+
+    @SuppressWarnings("unchecked")
+    public BoundedBuffer(int size) {
+        buffer = (T[]) new Object[size];
+    }
+
+    public synchronized void put(T item) throws InterruptedException {
+        // TODO: Реализуйте метод
+
+        if (count == buffer.length) {
+            wait();
+        }
+        else {
+            buffer[in] = item;
+            count++;
+            in++; out++;
+            notifyAll();
+        }
+    }
+
+    public synchronized T take() throws InterruptedException {
+        // TODO: Реализуйте метод
+        T object;
+
+        while (count == 0) {
+            wait();
+        }
+
+        object = buffer[out];
+        count--;
+        out--; in--;
+        notifyAll();
+
+        return object;
+    }
+}
