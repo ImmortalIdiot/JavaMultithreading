e78f6f17a97b45098a69199b44aab86aefb4d2a9
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-18 23:56:43 +0300

    feat: add buffer demo

---
+++ src/buffer/BoundedBufferTest.java
@@ -0,0 +1,21 @@
+package buffer;
+
+import java.util.concurrent.CountDownLatch;
+
+public class BoundedBufferTest {
+    public static void main(String[] args) throws InterruptedException {
+        BoundedBuffer<Integer> buffer = new BoundedBuffer<>(50);
+        int operations = 10;
+
+        CountDownLatch start = new CountDownLatch(1);
+        CountDownLatch finish = new CountDownLatch(2 * operations);
+
+        for (int i = 0; i < operations; i++) {
+            new Thread(new IntegerPutter(buffer, start, finish));
+            new Thread(new IntegerTaker(buffer, start, finish));
+        }
+
+        start.countDown();
+        finish.await();
+    }
+}
